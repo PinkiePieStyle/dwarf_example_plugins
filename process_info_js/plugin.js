3029ec387c4cbf96c469b068c88194964961585f
PinkiePieStyle <svenw1980@gmail.com>
2020-01-09 06:46:13 +0100

    processInfo via js example

---
+++ process_info_js/plugin.js
@@ -0,0 +1,40 @@
+/**
+    Dwarf - Copyright (C) 2019 Giovanni Rocca (iGio90)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+**/
+
+console.log("ProcessInfo via JS Example: Script loaded");
+
+// our script is loaded after core is attached so we have something to show on js side
+var processInfo = Dwarf.getProcessInfo();
+console.log(JSON.stringify(processInfo));
+
+/*
+    console shows something like this
+
+    ProcessInfo via JS Example: Script loaded
+    {
+        "name":"target.exe",
+        "spawned":true,
+        "pid":2388,
+        "threadId":15828,
+        "architecture":1, // here: ARCH_X86 -> see Documentation or DwarfArch in dwarfcore/src/consts.ts
+        "platform":1, // here: OS_WINDOWS -> see Documentation or DwarfPlatform in dwarfcore/src/consts.ts
+        "page_size":4096,
+        "pointer_size":4,
+        "java_available":false,
+        "objc_available":false
+    }
+*/
---
+++ process_info_js/plugin.py
@@ -0,0 +1,40 @@
+"""
+    Dwarf - Copyright (C) 2019 Giovanni Rocca (iGio90)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from dwarf_debugger.lib.core.dwarf_core import DwarfCore
+from dwarf_debugger.lib.core.dwarf_api import DwarfApi
+from dwarf_debugger.lib.core.dwarf_breakpoints_manager import DwarfBreakpointsManager
+
+
+class DwarfPlugin:
+    # simple dwarfplugin
+
+    # we extend plugin_info with our script to load 'script': 'path_to_script'
+    plugin_info = {
+        'name': 'ProcessInfo via JS Example',
+        'description': 'Prints ProcessInfo from debugged process to console',
+        'version': '1.0.0',
+        'author': 'Dwarf',
+        'homepage': 'https://github.com/iGio90/Dwarf',
+        'license': 'https://www.gnu.org/licenses/gpl-3.0',
+        'min_dwarf': '2.0.0',
+        'script': 'plugin.js'
+    }
+
+    def __init__(self, dwarf_core: DwarfCore):
+        # js only
+        pass
